87fa0b0622ed26390dbf3fff81a2e67c5ebb00c2
deSpasito6666 <diasgainutdinov666@gmail.com>
2024-11-22 18:27:25 +0300

    Build1
    Begin (Base Program)

---
+++ ReshenieDZ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReshenieDZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
